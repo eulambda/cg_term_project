48a359d72767e8ec605f963516e83128de916b50
name <email@email.email>
2024-06-07 04:01:47 +0900

    add skeletal animation helper

--- cg_term_project/ui/data_loader.hpp
+++ cg_term_project/ui/data_loader.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <model.h>
+#include <animator.h>
 #include <string>
 
 template<typename T>
@@ -22,9 +23,32 @@ T* fetch() {
 	return ptr;
 }
 
+inline Animator& fetch_animator(std::string path);
+inline Animation& fetch_animation(std::string path);
 inline Model& fetch_model(std::string path);
 inline Curve& fetch_curve(std::string name);
 inline Shader& fetch_shader(std::string name);
+
+inline Animator& fetch_animator(std::string path) {
+	static std::map<std::string, Animator> lut{};
+	auto found = lut.find(path);
+	if (found == lut.end()) {
+		auto& animation = fetch_animation(path);
+		found = lut.try_emplace(path, &animation).first;
+	}
+	if (found == lut.end()) throw std::exception{ (path + ": failed to load animator").c_str() };
+	return found->second;
+}
+inline Animation& fetch_animation(std::string path) {
+	static std::map<std::string, Animation> lut{};
+	auto found = lut.find(path);
+	if (found == lut.end()) {
+		auto& model = fetch_model(path);
+		found = lut.try_emplace(path, path, &model).first;
+	}
+	if (found == lut.end()) throw std::exception{ (path + ": failed to load animation").c_str() };
+	return found->second;
+}
 
 inline Model& fetch_model(std::string path) {
 	static std::map<std::string, Model> lut{};
@@ -38,7 +62,7 @@ inline Curve& fetch_curve(std::string name) {
 	static std::map<std::string, Curve> lut{};
 	auto found = lut.find(name);
 	if (found == lut.end()) found = lut.emplace(name, Curve{}).first;
-	if (found == lut.end()) throw std::exception{ (name + ": failed to fetch animation").c_str() };
+	if (found == lut.end()) throw std::exception{ (name + ": failed to fetch curve").c_str() };
 	return lut[name];
 }
 
